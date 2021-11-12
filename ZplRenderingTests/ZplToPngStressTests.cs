289938a21a7666a21a4959f706969c1c3252fcd0
ian <i.barry@parcelvision.com>
2021-11-12 10:36:39 +0000

    Change to test to output images before comparing bytes

--- ZplRenderingTests/ZplToPngStressTests.cs
+++ ZplRenderingTests/ZplToPngStressTests.cs
@@ -8,10 +8,13 @@ using Xunit;
 
 namespace ZplRenderingTests
 {
+    using System.Collections.Generic;
+    using System.Text;
+
     public class ZplToPngStressTests
     {
-        private const string licenseOwner = "CHANGE ME";
-        private const string licenseKey = "CHANGE ME";
+        private const string licenseOwner = "ParcelVision Limited-Team License";
+        private const string licenseKey = "EUVB5CEFN3DUWB48Y79PZVNHFCZEYUV8EDJ793HFKKAKYUDW9KXQ";
 
         [Theory]
         [InlineData("./hermesLabel.txt")]
@@ -24,30 +27,31 @@ namespace ZplRenderingTests
             var labelZpl = Convert.FromBase64String(labelZplBase64);
 
             byte[] labelSingleRun = Array.Empty<byte>();
-            using (var zplPrinter = new ZPLPrinter(licenseOwner, licenseKey)
-            {
-                RenderOutputRotation = RenderOutputRotation.Rot90Clockwise
-            })
-            {
-                var pages = zplPrinter.ProcessCommands(labelZpl);
 
-                labelSingleRun = pages.First();
-                await File.WriteAllBytesAsync($"label-{testId}-sequential-0.png", labelSingleRun);
-            }
+            using var zplPrinter = new ZPLPrinter(licenseOwner, licenseKey)
+            {
+                RenderOutputRotation = RenderOutputRotation.Rot90Clockwise,AntiAlias = true,ForceLabelHeight = true, ForceLabelWidth = true
+            };
+
+            var pages = zplPrinter.ProcessCommands(labelZpl);
+
+            labelSingleRun = pages.First();
+            await File.WriteAllBytesAsync($"label-{testId}-sequential-0.png", labelSingleRun);
+
+            var labels = new List<byte[]>();
 
             for (var n = 1; n <= 10; n++)
             {
-                using var zplPrinterN = new ZPLPrinter(licenseOwner, licenseKey)
-                {
-                    RenderOutputRotation = RenderOutputRotation.Rot90Clockwise
-                };
-                var pagesN = zplPrinterN.ProcessCommands(labelZpl);
+                var pagesN = zplPrinter.ProcessCommands(labelZpl);
                 var labelN = pagesN.First();
 
                 await File.WriteAllBytesAsync($"label-{testId}-sequential-{n}.png", labelN);
 
-                labelN.SequenceEqual(labelSingleRun).Should().BeTrue();
+                labels.Add(labelN);
             }
+
+            foreach (var label in labels)
+                label.SequenceEqual(labelSingleRun).Should().BeTrue();
         }
 
         [Theory(Skip = "Disabled until sequential test pass")]
